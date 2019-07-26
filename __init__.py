f64fd7e34dfccde922258eaa06b47a7952a7543e
Uriel Deveaud <uriel.deveaud@gmail.com>
2019-07-25 22:28:53 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -15,11 +15,81 @@ bl_info = {
 import os
 import bpy
 
-from bpy.types import (Panel, Operator, Menu, UIList)
+from bpy.types import (Panel, Operator, Menu, UIList, AddonPreferences)
+
+from . import addon_updater_ops
 
 # ##########################################################
 # PATH
 # ##########################################################
+
+
+
+class DemoPreferences(bpy.types.AddonPreferences):
+	bl_idname = __package__
+
+	# addon updater preferences
+
+	auto_check_update = bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False,
+		)
+	updater_intrval_months = bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0
+		)
+	updater_intrval_days = bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31
+		)
+	updater_intrval_hours = bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23
+		)
+	updater_intrval_minutes = bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59
+		)
+
+	def draw(self, context):
+		layout = self.layout
+		# col = layout.column() # works best if a column, or even just self.layout
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# updater draw function
+		# could also pass in col as third arg
+		addon_updater_ops.update_settings_ui(self, context)
+
+		# Alternate draw function, which is more condensed and can be
+		# placed within an existing draw function. Only contains:
+		#   1) check for update/update now buttons
+		#   2) toggle for auto-check (interval will be equal to what is set above)
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+		# Adding another column to help show the above condensed ui as one column
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
+
+
+
+
+
+
+
 
 # Manage path to snippets folders
 current_dir = os.path.dirname(__file__)
@@ -348,6 +418,7 @@ def draw_snippets_menu(self, context):
 # ##########################################################
 		
 classes = (
+	DemoPreferences,
 	TEXT_MT_snippets_menu_basics,
 	TEXT_MT_snippets_menu_uidesign,
 	TEXT_MT_snippets_menu_operators,
@@ -358,6 +429,8 @@ classes = (
 
         
 def register():
+
+	addon_updater_ops.register(bl_info)
 
 	for menu in submenus_basics:
 		bpy.utils.register_class(menu)
